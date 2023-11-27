9578bad437fda2b620c35f455ba0f997e8053210
Rodent12 <jainsayyam1234@gmail.com>
2023-11-27 16:06:47 +0530

    // first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [quote,setQuote] = useState("");
+
+  const fetchQuote = ()=>{
+    axios.get("https://api.adviceslip.com/advice")
+    .then((res)=>{
+      const {advice} = res.data.slip;
+      setQuote(advice);
+      console.log(advice);
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {fetchQuote()}
+      <div className='quote'>
+        {quote}
+      </div>
     </div>
   );
 }
